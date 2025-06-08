474486e878154a091a16cdec42346bfbf502d1cf
Ali Haghighatkhah <ali@muzikie.com>
2025-06-09 00:17:08 +0200

    Deploy MelodyneV13 to lisk-sepolia: 0xc372BbC62Fa1BdD334a9eE2448bD5C6C0D088d39

--- contracts/Melodyne.sol
+++ contracts/Melodyne.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IMelodyneConfig.sol";
 
 contract Melodyne {
-    string public constant VERSION = "12";
+    string public constant VERSION = "13";
     using SafeERC20 for IERC20;
     IERC20 public immutable usdc;
     IMelodyneConfig public immutable config;
