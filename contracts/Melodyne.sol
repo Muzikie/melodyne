764f24f0404ef0656c9252f5253f4ebeb173fd92
Ali Haghighatkhah <ali@muzikie.com>
2025-06-15 11:17:00 +0200

    ci:deploy MelodyneV1 to lisk: 0x5c4EfD5C0dD205067eCEeE0dDea025b08535052F

--- contracts/Melodyne.sol
+++ contracts/Melodyne.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IMelodyneConfig.sol";
 
 contract Melodyne {
-    string public constant VERSION = "16";
+    string public constant VERSION = "1";
     using SafeERC20 for IERC20;
     IERC20 public immutable usdc;
     IMelodyneConfig public immutable config;
---
+++ ignition/modules/MelodyneV1.ts
@@ -0,0 +1,11 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import deployments from '../../deploy-config.json';
+
+const NETWORK = "lisk";
+const USDC_ADDRESS = deployments[NETWORK].USDC;
+const CONFIG_ADDRESS = deployments[NETWORK].MelodyneConfig;
+
+export default buildModule("MelodyneV1", (m) => {
+  const melodyne = m.contract("Melodyne", [USDC_ADDRESS, CONFIG_ADDRESS]);
+  return { melodyne };
+});
